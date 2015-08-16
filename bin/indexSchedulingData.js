7c112e14c3661ca44c899951bc26762b3630bfcb
Paul Tomchik <pauljtomchik@gmail.com>
2015-08-16 14:08:59 -0400

    Moved code from exploration repo. Minimally tested here.

---
+++ bin/indexSchedulingData.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+
+'use strict';
+
+//TODO: Need to extract only the relevant data from the GTFS data.
+//      Otherwise, JSON object is about 185M.
+
+var fs          = require('fs'),
+    Converter   = require("csvtojson").Converter,
+    _           = require('lodash'),
+
+
+    // FIXME: hardcoded date
+    gtfsDataDir = __dirname + '/' + '../MTA_Subway_GTFS_Data/20150614/',
+
+    dataDict    = {},
+
+    tablePKs    = {
+        agency   : 'agency_id'  ,
+        calendar : 'service_id' ,
+        routes   : 'route_id'   ,
+        stops    : 'stop_id'    ,
+        trips    : 'trip_id'    ,
+    },
+
+    tables = Object.keys(tablePKs);
+
+
+console.log(module.parent);
+
+(function iterateAndParse (i) {
+    var tableName = tables[i],
+        filePath  = gtfsDataDir + tableName + '.txt',
+        converter,
+        fileStream;
+
+    if (i === tables.length) {
+        fs.writeFile(gtfsDataDir + 'indexedSchedulingData.json', JSON.stringify(dataDict, null, 4));
+        return;
+    }
+
+    converter  = new Converter({constructResult:true});
+    fileStream = fs.createReadStream(filePath);
+
+    converter.on("end_parsed", function (parsedTable) {
+        dataDict[tableName] = _.indexBy(parsedTable, function (rowObj) {
+            var keyName = tablePKs[tableName],
+                key;
+
+            if (tableName === 'trips') {
+                key = rowObj[keyName];
+                return key.substring(9, key.lastIndexOf('.') + 2);
+            }
+
+            return rowObj[keyName];
+        });
+        iterateAndParse(++i); 
+    });
+
+    fileStream.pipe(converter);
+}(0));
---
+++ tests/testLoadGTFS_Wrapper.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var wrapper = require('../lib/GTFS_Wrapper.js');
+
+console.log(JSON.stringify(wrapper, null, '    '));
+
